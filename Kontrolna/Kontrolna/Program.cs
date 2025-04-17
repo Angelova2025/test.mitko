c2cc75b52199eabc9fe801db19666ab9c032d770
Mitkoandonov <dimitarandonov2009@gmail.com>
2025-04-17 12:51:35 +0300

    Add files via upload

---
+++ Kontrolna/Kontrolna/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Kontrolna;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Въведи числа:");
+        string input = Console.ReadLine();
+        
+        int[] numbers = new int[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            numbers[i] = input[i] - '0';
+        }
+        
+        int sum = 0;
+        int max = numbers[0];
+        int countOfNines = 0;
+        
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+
+            if (numbers[i] == 9)
+            {
+                countOfNines++;
+            }
+        }
+        
+        Console.WriteLine("Сума на елементи: " + sum);
+        Console.WriteLine("Най-голям елемент: " + max);
+        Console.WriteLine("Числото 9 се среща " + countOfNines + " пъти");
+    }
+}
